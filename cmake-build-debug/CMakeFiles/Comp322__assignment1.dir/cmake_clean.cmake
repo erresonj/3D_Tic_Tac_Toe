5796f3b3ca0d326f075dd75c0f88baac76c9abef
erresonj <erresonj@gmail.com>
2020-07-04 13:49:00 -0400

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Comp322__assignment1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Comp322__assignment1.dir/main.cpp.o"
+  "Comp322__assignment1.exe"
+  "Comp322__assignment1.pdb"
+  "libComp322__assignment1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Comp322__assignment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
